c81fd686714dc182ec2ce26cc62a2f3a7b1d169f
Михеева Евгения <mikheevaea@it2g.ru>
2020-11-04 16:48:17 +0300

    binary-search

--- __tests__/binary-search/binary-search.test.ts
+++ __tests__/binary-search/binary-search.test.ts
@@ -6,6 +6,7 @@ test('Binary search should work correctly with an array', () => {
     expect(binarySearch([1, 3, 5, 6, 7, 8, 10], 8)).toBe(5);
     expect(binarySearch([1, 3, 5, 6, 7, 8, 10], 1)).toBe(0);
     expect(binarySearch([1, 3, 5, 6, 7, 8, 10], 10)).toBe(6);
+    expect(binarySearch([1], 1)).toBe(0);
 });
 
 test('Binary search should work correctly if the array does not contain a number ', () => {
